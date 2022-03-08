48e60a946361f4cc20a6845dd905b33aabbde79d
eladsez <eladsez36@gmail.com>
2022-03-08 07:28:56 -0800

    test complete

--- mat.cpp
+++ mat.cpp
@@ -3,5 +3,9 @@
 #include "mat.hpp"
 
 std::string ariel::mat(int col, int row , char sym1, char sym2){
+    // char mat[row][col];
+    // for (int i =0; i < col; ++i){
+        
+    // }  
     return "a";
 }
